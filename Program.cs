e7a635ce798bab70a7b5780004de98d8d789d467
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:13:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi = 1;
+string programcininAdi;
+Console.WriteLine("Adınızı yazar mısınız ? ");
+programcininAdi = Convert.ToString(Console.ReadLine());
+//aynı ismi yazacak 10 defa  sayi 10 a eşit olana kadar
+while (sayi<=10) 
+{
+    Console.WriteLine("Programcının Adı : " + programcininAdi);
+    sayi++;
+}
